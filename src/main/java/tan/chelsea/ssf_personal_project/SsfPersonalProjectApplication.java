84a9af7361a92291ebcad187f7b0a62ffe1131e1
Heavy-Bubbles <chelseatanxuanlin@gmail.com>
2023-05-17 21:59:13 +0800

    first commit

---
+++ src/main/java/tan/chelsea/ssf_personal_project/SsfPersonalProjectApplication.java
@@ -0,0 +1,13 @@
+package tan.chelsea.ssf_personal_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfPersonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfPersonalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tan/chelsea/ssf_personal_project/SsfPersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tan.chelsea.ssf_personal_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfPersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
